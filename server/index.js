9efb165cbc1bc0c921feef53aae75f6383c9d924
HsinHsieh <sam786890@gmail.com>
2017-12-09 16:23:45 +0800

    Router 分配

---
+++ server/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const path = require('path');
+const url = require('url');
+
+module.exports = class {
+
+    constructor(router) {
+
+        this.router = router;
+        // this.productManager = new ProductManager();
+        this.SetAPI();
+    }
+
+    SetAPI() {
+
+        this.router.get("/", function(req, res) {
+            res.sendfile('./UI/index.html', function(err) {
+                if (err) res.send(404);
+            });
+            // res.end(JSON.stringify({success:true , data:result}));
+        });
+
+        this.router.get(/(.*)\.(jpg|gif|png|ico|woff|ttf|txt|html|css|js)/i, function(req, res) {
+            res.sendfile("./UI" + req.params[0] + "." + req.params[1], function(err) {
+                if (err) res.send(404);
+            });
+        });
+    }
+}
--- wgs.js
+++ wgs.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const http = require('http');
 
+const Index = require('./server/index.js');
 
 var app = express();
 
+var adminRouter = express.Router();
+var indexRouter = express.Router();
+var product = express.Router();
+var loginRouter = express.Router();
+var shoppingCartRouter = express.Router();
 
-app.get("/", function(req, res) {
-    res.sendfile('./UI/index.html', function(err) {
-        if (err) res.send(404);
-    });
-});
+new Index(indexRouter);
+app.use('/', indexRouter);
 
-app.get(/(.*)\.(jpg|gif|png|ico|woff|ttf|txt|html|css|js)/i, function(req, res) {
-    res.sendfile("./UI" + req.params[0] + "." + req.params[1], function(err) {
-        if (err) res.send(404);
-    });
-});
+// app.get("/", function(req, res) {
+//     res.sendfile('./UI/index.html', function(err) {
+//         if (err) res.send(404);
+//     });
+// });
+//
+// app.get(/(.*)\.(jpg|gif|png|ico|woff|ttf|txt|html|css|js)/i, function(req, res) {
+//     res.sendfile("./UI" + req.params[0] + "." + req.params[1], function(err) {
+//         if (err) res.send(404);
+//     });
+// });
+
 var server = app.listen(3000, function() {
-  console.log('Listening on port 3000');
- });
+    console.log('Listening on port 3000');
+});
