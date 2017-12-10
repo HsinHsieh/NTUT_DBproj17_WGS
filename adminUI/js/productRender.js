801e89d971687b1116f835f937977cfdfa2c9c73
zz246841 <zz246841@gmail.com>
2017-12-10 21:16:26 +0800

    product_search_filter

--- adminUI/js/productRender.js
+++ adminUI/js/productRender.js
@@ -1,11 +1,14 @@
 // const product_search_row = "'<tr><th scope='row'>1</th<td>ACT0000007</td><td>{{Product_Name}}</td><td>{{Category}}</td><td>{{Price}}</td><td><button class='btn btn-primary' type='button' name='button' onclick='location.href='product_edit.html''>編輯</button></td><td><button class='btn btn-danger' type='button' name='button'>刪除</button></td></tr>"
 $(document).ready(function() {
   var productRow = function(msg) {
+    var resStr = "";
     var resObj = JSON.parse(msg);
+    //$("#productSearch_catagory").reset();
     for (var i = 0; i < Object.keys(resObj).length; i++) {
-      var resStr = "'<tr><th scope='row'>" + (i + 1) + "</th><td>" + resObj[i].PID + "</td><td>" + resObj[i].Product_Name + "</td><td>" + resObj[i].Supplier + "</td><td>" + resObj[i].Price + "</td><td><button class='btn btn-primary' type='button' name='button' onclick='location.href='product_edit.html''>編輯</button></td><td><button class='btn btn-danger' type='button' name='button'>刪除</button></td></tr>"
-      $(".product_list").append(resStr);
+      resStr += "'<tr><th scope='row'>" + (i + 1) + "</th><td>" + resObj[i].PID + "</td><td>" + resObj[i].Product_Name + "</td><td>" + resObj[i].Supplier + "</td><td>" + resObj[i].Price + "</td><td><button class='btn btn-primary' type='button' name='button' onclick='location.href='product_edit.html''>編輯</button></td><td><button class='btn btn-danger' type='button' name='button'>刪除</button></td></tr>"
     }
+    console.log(resStr);
+    $(".product_list").html(resStr);
   }
   var searchCatagory = function(msg) {
     var resObj = JSON.parse(msg);
@@ -19,16 +22,13 @@ $(document).ready(function() {
 
   $("#productSearch").click(function() {
     var data = {
-      "CID": ($('#productSearch_CID').val()),
+      "PID": ($('#productSearch_PID').val()),
       "Name": ($('#productSearch_name').val()),
-      "Price": ($('#productSearch_price_up').val()),
+      "Supplier": ($('#productSearch_supplier').val()),
+      "Price_low": ($('#productSearch_price_low').val()),
+      "Price_up": ($('#productSearch_price_up').val()),
+      "Category": ($('#productSearch_catagory').val()),
     };
     Post('/admin/api/productSearch', data, productRow);
-    // console.log($('#productSearch_CID').val());
-    // console.log($('#productSearch_name').val());
-    // console.log($('#productSearch_supplier').val());
-    // console.log($('#productSearch_price_up').val());
-    // console.log($('#productSearch_price_low').val());
-    // console.log($('#productSearch_catagory').val());
   });
 });
--- server/admin.js
+++ server/admin.js
@@ -18,6 +18,12 @@ module.exports = class {
       });
       // res.end(JSON.stringify({success:true , data:result}));
     });
+    this.router.get("/:page", function(req, res) {
+      res.sendfile('./adminUI/' + req.params.page + '.html', function(err) {
+        if (err) res.send(404);
+      });
+      // res.end(JSON.stringify({success:true , data:result}));
+    });
     this.router.get("/api/productCatagory", function(req, res) {
       var callback = function(msg) {
         res.send(msg);
@@ -33,11 +39,18 @@ module.exports = class {
       // res.end(JSON.stringify({success:true , data:result}));
     });
     this.router.post("/api/productSearch", function(req, res) {
-      console.log(req.body);
+      //console.log(req.body);
       var callback = function(msg) {
         res.send(msg);
       };
-      //(new sql(data)).ReturnJson(callback);
+      console.log(req.body);
+      var sqlPID = req.body.PID == '' ? "" : (" `PID` = '" + req.body.PID + "'");
+      var sqlName = req.body.Name == '' ? "" : (req.body.PID == '' ? "" : " AND ") + " `Product_Name` LIKE '%" + req.body.Name + "%'";
+      var sqlPrice = req.body.Price_low == '' ? "" : (req.body.Name == '' ? "" : " AND ") + " `Price` BETWEEN " + req.body.Price_low + " AND " + req.body.Price_up;
+      var sqlSupplier = req.body.Supplier == '' ? "" : (req.body.Price_up == '' ? "" : " AND ") + " `Supplier` LIKE '" + req.body.Supplier + "'";
+      var sqlStr = "SELECT *  FROM `product` WHERE" + sqlPID + sqlName + sqlPrice + sqlSupplier;
+      console.log(sqlStr);
+      (new sql(sqlStr)).ReturnJson(callback);
     });
   };
 
