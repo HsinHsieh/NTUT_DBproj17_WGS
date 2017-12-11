37fbba59f678669831c53f3cbf6d18dd39d22e6e
zz246841 <zz246841@gmail.com>
2017-12-11 11:36:59 +0800

    admin-index-productNum

--- server/adminApi.js
+++ server/adminApi.js
@@ -16,19 +16,22 @@ module.exports = class {
       (new sql("SELECT * FROM `category`ORDER BY CAID ASC")).ReturnJson(callback);
       // res.end(JSON.stringify({success:true , data:result}));
     });
+    this.router.get("/mainProductNum", function(req, res) {
+      var callback = function(msg) {
+        res.send(msg);
+      };
+      (new sql("SELECT count(*) AS Pnum FROM product")).ReturnJson(callback);
+    });
     this.router.get("/productSupplier", function(req, res) {
       var callback = function(msg) {
         res.send(msg);
       };
       (new sql("SELECT DISTINCT Supplier FROM `product`ORDER BY Supplier ASC")).ReturnJson(callback);
-      // res.end(JSON.stringify({success:true , data:result}));
     });
     this.router.post("/productSearch", function(req, res) {
-      //console.log(req.body);
       var callback = function(msg) {
         res.send(msg);
       };
-
       var sqlPID = (req.body.PID == '') ? "" : " `PID` = '" + req.body.PID + "'" + " AND ";
       var sqlName = " `Product_Name` LIKE '%" + req.body.Name + "%'";
       var sqlPrice = (req.body.Price_low == '') ? "" : " AND " + " `Price` BETWEEN " + req.body.Price_low + " AND " + req.body.Price_up;
