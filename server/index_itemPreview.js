ab7c7e3ec25a2a1b88070ea839559ce45465b77f
HsinHsieh <sam786890@gmail.com>
2017-12-12 11:39:40 +0800

    home index new arrival 完成

--- server/index_itemPreview.js
+++ server/index_itemPreview.js
@@ -1,11 +1,11 @@
 const DataBaseController = require('../DB/DatabaseController.js')
 
-const item_preview_template = "<div class='col-xs-12 col-sm-6 col-md-3'><div class='pro-text text-center'><div class='pro-img'><img src='./product_pic/{{Product_Name}}.jpg ' alt='2' /></div><div class='pro-text-outer'><span>{{Category}}</span><a href='{{url}}'><h4>{{Product_Name}}</h4></a><p class='wk-price'>{{Price}}</p><a href='#' class='add-btn'>Add to cart</a></div></div></div>"
+const item_preview_template = "<div class='col-xs-12 col-sm-6 col-md-4'><div class='pro-text text-center'><div class='pro-img'><img src='./product_pic/{{Product_Name}}.jpg ' alt='幹!找不到圖片' width='200' height='300' /></div><div class='pro-text-outer'><span>{{Category}}</span><a href='{{url}}'><h4>{{Product_Name}}</h4></a><p class='wk-price'> NT$ {{Price}}</p><a href='#' class='add-btn'>Add to cart</a></div></div></div>"
 
 module.exports = class {
     constructor() {
         this.db = DataBaseController.GetDB();
-        this.data = null;
+        this.data = "";
     }
 
     // AddItemPreview(attribute, callback) {
@@ -19,18 +19,34 @@ module.exports = class {
     //     })
     // }
 
+    // this.db.query("SELECT * FROM `product` ORDER BY `product`.`Launch_Date` ASC LIMIT 16", function(error, rows, fields) {
+    //     //檢查是否有錯誤
+    //     if (error)
+    //         throw error;
+    //     this.item_preview = "";
+    //     for (i = 0; i < row.count; i++) {
+    //         this.item_preview = item_preview_template //replace("{{num}}",i+1)
+    //             .replace("{{Product_Name}}", rows[i].Product_Name)
+    //             .replace("{{Product_Name}}", rows[i].Product_Name)
+    //             .replace("{{Category}}", rows[i].Category)
+    //             .replace("{{Price}}", rows[i].Price);
+    //     }
+
 
     AddItemPreview(callback) {
-        this.db.query("SELECT * FROM `product` WHERE `Product_Name` LIKE 'mountain'", function(error, rows, fields) {
+        this.db.query("SELECT * FROM `product` ORDER BY `product`.`Launch_Date` ASC LIMIT 16", function(error, rows, fields) {
             //檢查是否有錯誤
             if (error)
                 throw error;
-            this.data = rows[0];
-            this.item_preview = item_preview_template //replace("{{num}}",i+1)
-                .replace("{{Product_Name}}", this.data.Product_Name)
-                .replace("{{Product_Name}}", this.data.Product_Name)
-                .replace("{{Category}}", this.data.Category)
-                .replace("{{Price}}", this.data.Category);
+            this.item_preview = "";
+            for (var i = 0; i < rows.length; i++) {
+                this.data = rows[i];
+                this.item_preview += item_preview_template //replace("{{num}}",i+1)
+                    .replace("{{Product_Name}}", this.data.Product_Name)
+                    .replace("{{Product_Name}}", this.data.Product_Name)
+                    .replace("{{Category}}", this.data.Category)
+                    .replace("{{Price}}", this.data.Price);
+            }
             callback(this.item_preview);
         })
     }
