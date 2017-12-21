f4504641c517f4c5aa2a24d453ec1d4533d41a33
HuYuHsiang <nightlord851108@gmail.com>
2017-12-21 15:45:17 +0800

    Finish adding and loading shopping cart.

--- UI/javascript/shoppingCartRender.js
+++ UI/javascript/shoppingCartRender.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-    GetInfo();
+    GetItems();
 });
+
+
+function Calculate() {
+    var total = 0;
+    function CartTotal(){
+
+    };
+}
 
 function GetInfo() {
     var apiUrl = '/shopping_cart/get';
@@ -8,4 +16,27 @@ function GetInfo() {
         $("#CartList").html(msg);
     };
     Get(apiUrl, callback);
+}
+
+function PutItems() {
+    var url = '/shopping_cart/add';
+    var data = {
+        pid: 'ACT0000015',
+        customer: 'wasd'
+    }
+    var callback = function(msg) {
+        alert(msg)
+    }
+    Post(url, data, callback);
+}
+
+function GetItems() {
+    var url = '/shopping_cart/items';
+    var data = {
+        customer:'wasd',
+    };
+    var callback = function(msg) {
+        $("#CartList").html(msg);
+    };
+    Post(url, data, callback);
 }
