fd8944f65a22d882231dd05b9c9c2d06466e408d
mario31274 <mario31274@yahoo.com.tw>
2017-12-23 23:32:43 +0800

    productKeys 前端 微調

--- UI/javascript/productKeysRender.js
+++ UI/javascript/productKeysRender.js
@@ -23,7 +23,25 @@ function CheckLogin() {
 function GetProductKeys(CID) {
     var apiUrl = '/productKeys/' + CID
     var callback = function(product_keys) {
-        console.log(product_keys)
+        // console.log(product_keys)
+        for (var i = 0; i < Object.keys(product_keys).length; i++) {
+          if (product_keys[i].Key_Used == 0){
+              product_keys[i].Key_Status = "<button class='btn btn-primary btn-block' type='button' name='button' onclick='RevealProductKey()'>啟用金鑰</button>"
+          }
+          else {
+              product_keys[i].Key_Status = "<p class='form-control' align='center'>" + product_keys[i].License_Key + "</p>"
+          }
+        }
+        var resStr = "";
+        for (var i = 0; i < Object.keys(product_keys).length; i++) {
+          resStr += "<tr><td><p class='form-control-static'>" + product_keys[i].Order_Number + "</p></td><td><p class='form-control-static'>" + product_keys[i].Product_Name + "</p></td><td><p class='form-control-static'>" + product_keys[i].Formated_Order_Time + "</p></td><td id='key_status'>"+ product_keys[i].Key_Status +"</td></tr>";
+        }
+        $(".product_list").html(resStr);
     }
     Get(apiUrl, callback);
+};
+
+function RevealProductKey(){
+  $("#key_status").hide();
+  console.log("Revealed")
 };
--- server/productKeys_db.js
+++ server/productKeys_db.js
@@ -7,11 +7,17 @@ module.exports = class {
     }
 
     GetProductKeysInfo(callback){
-        var command = "SELECT `Order_Number`, `Product_Name`, `Order_Time`, `License_Key`, `Key_Used` FROM (`order_content` JOIN `order_main` ON `order_content`.`Order_Number` = `order_main`.`OID`) JOIN `product` ON `order_content`.`Item` = `product`.`PID` WHERE `Order_Number` IN (SELECT `OID` FROM `order_main` WHERE `Customer` = '" + this.id + "')";
+        var command = "SELECT `Order_Number`, `Product_Name`, DATE_FORMAT(Order_Time,'%Y-%m-%d %k:%i:%s') AS Formated_Order_Time, `License_Key`, `Key_Used` \
+                       FROM (`order_content` \
+                                JOIN `order_main` ON `order_content`.`Order_Number` = `order_main`.`OID`) \
+                                JOIN `product` ON `order_content`.`Item` = `product`.`PID` \
+                       WHERE `Order_Number` IN (SELECT `OID` \
+                                                FROM `order_main` \
+                                                WHERE `Customer` = '" + this.id + "' \
+                                                AND `order_main`.`Status` = 1)";
         this.db.query(command, function(error, rows, fields) {
             if (error)
                 throw error;
-            console.log(rows)
             callback(rows);
         });
     }
