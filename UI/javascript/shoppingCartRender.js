9fc1b7b3b5be0837e9a6a2cc86210108ffb59149
HuYuHsiang <nightlord851108@gmail.com>
2017-12-23 15:56:00 +0800

    Completed clear, total, checkout functions.

--- UI/javascript/shoppingCartRender.js
+++ UI/javascript/shoppingCartRender.js
@@ -1,30 +1,62 @@
 $(document).ready(function() {
-    GetItems();
+    GetInfo();
+
+    $("#clear").click(function() {
+        clear();
+    });
+
+    $("#checkout").click(function() {
+        checkout();
+    });
+
+    function checkout(){
+        var url = '/shopping_cart/checkout';
+        var data = {
+            customer: 'wasd',
+        };
+        var callback = function (msg) {
+            swal(msg);
+            GetInfo();
+        };
+        Post(url, data);
+    }
+
+    function clear() {
+        var url = '/shopping_cart/clear';
+        var data = {
+            customer: 'wasd'
+        };
+        var callback = function(msg) {
+            swal(msg);
+            GetInfo();
+        };
+        Post(url, data, callback);
+    }
+
+    function GetInfo() {
+        GetItems();
+    }
+
+    function GetItems() {
+        var url = '/shopping_cart/items';
+        var data = {
+            customer:'wasd',
+        };
+        var callback = function(msg) {
+            $("#CartList").html(msg);
+        };
+        Post(url, data, callback);
+        GetTotal(data);
+    }
+
+    function GetTotal(data) {
+        var url = '/shopping_cart/total';
+
+        var callback = function(msg) {
+            $("#total").html(msg);
+        }
+
+        Post(url, data, callback);
+    }
+
 });
-
-
-function Calculate() {
-    var total = 0;
-    function CartTotal(){
-
-    };
-}
-
-function GetInfo() {
-    var apiUrl = '/shopping_cart/get';
-    var callback = function(msg) {
-        $("#CartList").html(msg);
-    };
-    Get(apiUrl, callback);
-}
-
-function GetItems() {
-    var url = '/shopping_cart/items';
-    var data = {
-        customer:'wasd',
-    };
-    var callback = function(msg) {
-        $("#CartList").html(msg);
-    };
-    Post(url, data, callback);
-}
