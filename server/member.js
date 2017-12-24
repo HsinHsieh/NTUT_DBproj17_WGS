58e34a665b6598f3167d893050ce5b270a92f6fa
HsinHsieh <sam786890@gmail.com>
2017-12-24 17:56:13 +0800

    UI註冊系統完成

--- server/member.js
+++ server/member.js
@@ -14,4 +14,16 @@ module.exports = class {
                 callback(err, result[0]);
             });
     }
+
+    MemberRegister(RegisterData, callback) {
+        var command = "INSERT INTO `member` (`CID`, `Password`, `Type`, `First_Name`, `Last_Name`,`Phone`, `Gender`) VALUES ('" + RegisterData.CID + "','" + RegisterData.Password +
+            "','1','" + RegisterData.First_Name + "','" + RegisterData.Last_Name + "','" + RegisterData.Phone + "','" + RegisterData.Gender + "')";
+        //console.log(command);
+        this.db.query(
+            command,
+            function(err, result, fields) {
+                //console.log(result[0]);
+                callback(err, result);
+            });
+    }
 }
