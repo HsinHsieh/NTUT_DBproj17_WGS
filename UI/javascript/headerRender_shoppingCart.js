38df283575453375c6dd5cdddbb265962e10f278
HuYuHsiang <nightlord851108@gmail.com>
2017-12-24 15:25:43 +0800

    Display total price on header cart.

--- UI/javascript/headerRender_shoppingCart.js
+++ UI/javascript/headerRender_shoppingCart.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
         var callback = function (data) {
             result = "";
             for (var i = 0; i < data.length; i++) {
-                var p = data[i]
+                var p = data[i];
                 result += "<div class='cart-content'>\
                             <div class='col-sm-4 col-md-4'><a href='/product?pid=" + p.pic + "'><img src='./product_pic/"+ p.pic + ".jpg' alt='幹找不到圖片' style='width:68px; height:100px; display:inline-block;'></a></div>\
                             <div class='col-sm-8 col-md-8'> \
@@ -30,12 +30,19 @@ $(document).ready(function() {
                            </div>";
            }
             $("#HeaderCart").html(result);
+            GetTotal();
         };
         Post(url, data, callback)
     };
 
     function GetTotal () {
         var url = '/shopping_cart/total';
-        var callback
+        var data = {
+            customer: this.user,
+        };
+        var callback = function (msg) {
+            $("#header_cart_total").html(msg);
+        };
+        Post(url, data, callback)
     }
 });
