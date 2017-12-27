3dedbb65e0949424b5924a5351c5e624dba62003
mario31274 <mario31274@yahoo.com.tw>
2017-12-28 02:01:48 +0800

    addcomment post問題 未解決

--- UI/javascript/addCommentRender.js
+++ UI/javascript/addCommentRender.js
@@ -1,9 +1,46 @@
+
+
+function PostComment() {
+    var com = $("#commentform").serializeObject();
+    // console.log(com);
+    var pid = new URL(window.location.href).searchParams.get("pid");
+    // console.log(pid);
+    var apiUrl = '/submit' + pid;
+    var data = {
+      PID: pid,
+      comment: com["comment"],
+      rating: com["rating"]
+    };
+    var callback = function(msg) {
+        console.log(msg);
+    }
+    Post(apiUrl, data, callback);
+}
+
 $(document).ready(function() {
     var url = new URL(window.location.href);
     var pid = url.searchParams.get("pid");
 
     GetProductByID(pid);
     GetAccount();
+
+    $.fn.serializeObject = function() {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function() {
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+    $("#send").click(PostComment());
+    // $("#commentform").submit();
 });
 
 function GetAccount() {
@@ -29,7 +66,6 @@ function GetProductByID(id) {
     var callback = function(product_info) {
         $("#product_pic").attr("src", "./product_pic/" + product_info["PID"] + ".jpg");
         $("#product_name").html(product_info["Product_Name"]);
-        $("#commentform").attr("action", "/addcomment/submit/" + product_info["PID"]);
     }
     Get(apiUrl, callback);
 };
