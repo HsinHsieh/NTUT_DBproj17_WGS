811a7583d02788e48ff341a8c41f2e0adf9e32c0
Jiasen Li <fkl7619480@gmail.com>
2017-12-28 06:26:52 +0800

    CountingStart.....

--- UI/javascript/productMoreRender.js
+++ UI/javascript/productMoreRender.js
@@ -20,30 +20,32 @@ function GetCommentByID(id, index) {
     var callback = function(comment) {
         var resStr = "";
         for (var i = 0; i < Object.keys(comment).length; i++) {
-          //comment[i].Grade
-          resStr +=
-            "<li class='comment'>\
-                <div class='comment-body'>\
-                  <div class='well well-lg'>\
-                    <header class='text-left'>\
-                     <div class='star2'>\
-                       <ul>\
-                         <li class='comment-author text-uppercase reviews'><i class='fa fa-user'></i> " + comment[i].Customer + "</li>\
-                         <li class='yellow-color'><i class='fa fa-star' aria-hidden='true'></i></li>\
-                         <li class='yellow-color'><i class='fa fa-star' aria-hidden='true'></i></li>\
-                         <li class='yellow-color'><i class='fa fa-star' aria-hidden='true'></i></li>\
-                         <li class='yellow-color'><i class='fa fa-star' aria-hidden='true'></i></li>\
-                         <li><i class='fa fa-star' aria-hidden='true'></i></li>\
-                       </ul>\
+            var yellowStarts = function(Grade){
+                var result = "";
+                for (var i = 0; i <= Grade; i++) {
+                    result += "<li class='yellow-color'><i class='fa fa-star' aria-hidden='true'></i></li>";
+                }
+                for (var i = 0; i < 5-Grade; i++) {
+                    result += "<li><i class='fa fa-star' aria-hidden='true'></i></li>";
+                }
+                return result;
+            }(comment[i].Grade);;
+            resStr +=
+              "<li class='comment'>\
+                  <div class='comment-body'>\
+                    <div class='well well-lg'>\
+                      <header class='text-left'>\
+                       <div class='star2'>\
+                         <ul><li class='comment-author text-uppercase reviews'><i class='fa fa-user'></i> " + comment[i].Customer + "</li>" + yellowStarts + "</ul>\
+                       </div>\
+                       <time class='comment-date reviews'><i class='fa fa-clock-o'></i> " + comment[i].Comment_Time + "</time>\
+                       </header>\
+                       <div class='comment-comment'>\
+                         <p>" + comment[i].Comment_Text + "</p>\
+                       </div>\
                      </div>\
-                     <time class='comment-date reviews'><i class='fa fa-clock-o'></i> " + comment[i].Comment_Time + "</time>\
-                   </header>\
-                   <div class='comment-comment'>\
-                     <p>" + comment[i].Comment_Text + "</p>\
                    </div>\
-                 </div>\
-               </div>\
-             </li>";
+                 </li>";
         }
         $(".comment-list").html(resStr);
     }
