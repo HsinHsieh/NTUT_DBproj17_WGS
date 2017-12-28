97fc7186c96f287dc1772cc752dc90b0a0b93088
Jiasen Li <fkl7619480@gmail.com>
2017-12-29 02:59:08 +0800

    new comment Render

--- UI/javascript/productMoreRender.js
+++ UI/javascript/productMoreRender.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var pid = url.searchParams.get("pid");
 
     GetProductByID(pid, url.pathname);
-    //GetCommentByID(pid, url.pathname);
+    GetCommentByID(pid, url.pathname);
 });
 
 function GetProductByID(id, index) {
@@ -22,36 +22,73 @@ function GetCommentByID(id, index) {
             var resStr = "<div>本商品未存在任何評論 快來寫下你的評論吧!</div>";
         } else {
             var resStr = "";
-            for (var i = 0; i < Object.keys(comment).length; i++) {
-                var yellowStarts = function(Grade) {
-                    var result = "";
-                    for (var i = 0; i < Grade; i++) {
-                        result += "<li class='yellow-color'><i class='fa fa-star' aria-hidden='true'></i></li>";
-                    }
-                    for (var i = 0; i < 5 - Grade; i++) {
-                        result += "<li><i class='fa fa-star' aria-hidden='true'></i></li>";
-                    }
-                    return result;
-                }(comment[i].Grade);
-                resStr +=
-                    "<li class='comment'>\
-                      <div class='comment-body'>\
-                        <div class='well well-lg'>\
-                          <header class='text-left'>\
-                           <div class='star2'>\
-                             <ul><li class='comment-author text-uppercase reviews'><i class='fa fa-user'></i> " + comment[i].Customer + "</li>" + yellowStarts + "</ul>\
-                           </div>\
-                           <time class='comment-date reviews'><i class='fa fa-clock-o'></i> " + comment[i].Comment_Time + "</time>\
-                           </header>\
-                           <div class='comment-comment'>\
-                             <p>" + comment[i].Comment_Text + "</p>\
-                           </div>\
-                         </div>\
-                       </div>\
-                     </li>";
+            for (var i = 1; i <= Math.ceil(Object.keys(comment).length/3); i++) {
+                resStr += MakeCommentPage(i, comment);
             }
+            resStr += MakePageTags(Object.keys(comment).length);
         }
-        $(".comment-list").html(resStr);
+        $("#comment").html(resStr);
     }
     Get(apiUrl, callback);
 };
+
+function MakeCommentPage(pageNo, comment) {
+  var result = "";
+  if (pageNo == 1) {
+    result += "<div id='comment-page1' class='tab-pane fade in active'><ul>";
+  } else {
+    result += "<div id='comment-page" + pageNo + "' class='tab-pane fade'><ul>";
+  }
+  for (var i = 0; i < 3; i++) {
+    var index = (pageNo-1)*3 + i;
+    if(comment[index]) {
+      result += MakeCommentBox(comment[index].Customer, comment[index].Grade, comment[index].Comment_Time, comment[index].Comment_Text);
+    }
+  }
+  result += "</ul></div>"
+  return result;
+}
+
+function MakeCommentBox(author, grade, date, text) {
+  var yellowStarts = function(Grade) {
+      var result = "";
+      for (var i = 0; i < Grade; i++) {
+          result += "<li class='yellow-color'><i class='fa fa-star' aria-hidden='true'></i></li>";
+      }
+      for (var i = 0; i < 5 - Grade; i++) {
+          result += "<li><i class='fa fa-star' aria-hidden='true'></i></li>";
+      }
+      return result;
+  }(grade);
+  var comment =
+     "<li class='comment'>\
+        <div class='comment-body'>\
+          <div class='well well-lg'>\
+            <header class='text-left'>\
+             <div class='star2'>\
+               <ul><li class='comment-author text-uppercase reviews'><i class='fa fa-user'></i> " + author + "</li>" + yellowStarts + "</ul>\
+             </div>\
+             <time class='comment-date reviews'><i class='fa fa-clock-o'></i> " + date + "</time>\
+             </header>\
+             <div class='comment-comment'>\
+               <p>" + text + "</p>\
+             </div>\
+           </div>\
+         </div>\
+       </li>";
+  return comment;
+}
+
+function MakePageTags(commentNum) {
+  var result = "";
+  if (commentNum > 3) {
+    result += "<div class='col-xs-12 col-sm-12 col-md-12 text-right'>"
+    result += "<ul class='pagination'>";
+    result += "<li class='active'><a data-toggle='tab' href='#comment-page1'>1</a></li>"
+    for (var i = 2; i <= Math.ceil(commentNum/3); i++) {
+      result += "<li><a data-toggle='tab' href='#comment-page" + i + "'>" + i + "</a></li>";
+    }
+    result += "</ul>";
+  }
+  return result;
+}
